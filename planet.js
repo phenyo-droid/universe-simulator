53b6874b875cea95ca5ec9a449072655697cd9ba
phenyo-droid <pnkoana61@gmail.com>
2023-04-25 02:46:08 +0200

    set up

---
+++ planet.js
@@ -0,0 +1,15 @@
+
+
+class Planet {
+
+    constructor(radius, angle, distance){
+        this.angle = 0;
+        this.radius = r;
+        this.distance = d
+    }
+
+    draw(){
+
+        ellipse(0, 0, radius*2, radius*2)
+    }
+}
